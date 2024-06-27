b959111ab1b93552e8f8f71845aa16dd71dcab3c
anup thakur <anksthakur7@gmail.com>
2024-06-27 17:57:49 +0530

    github login

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
+import { FaGoogle } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 
 const page = () => {
   return (
     <>
-    <div className='bg-gray-300'>
-      <div className='bg-white w-4/5 mx-auto my-0 flex gap-24 px-9'>
+    <div className='bg-gray-300 min-h-96'>
+      <div className='bg-white w-4/5 mx-auto my-0 flex gap-24 px-9 h-96'>
         <div className='flex flex-col mt-8'>
       <h1 className=' font-bold text-center '>Sign in</h1>
-      <Link href="/" className='border-solid border-2 border-black-500 px-2 rounded-md text-center mt-3'>Sign in with Google</Link>
+      
+      <Link href="/api/auth/signin" className='flex justify-evenly border-solid border-2 border-black-500 px-2 rounded-md text-center mt-3'><span className='mt-1'><FaGithub color="black"/></span>Sign in with GitHub</Link>
+      <Link href="/api/auth/signin" className='flex justify-evenly border-solid border-2 border-black-500 px-2 rounded-md text-center mt-3'><span className='mt-1'><FaGoogle color="blue" /></span>Sign in with Google</Link>
       <p className='text-center mt-3'>--------- or ---------</p>
-      <Link href="/login" className='border-solid border-2 border-black-500 px-2 rounded-md bg-yellow-500 text-center mt-3'>Create a New Account</Link>
+      <Link href="/register" className='border-solid border-2 border-black-500 px-2 rounded-md bg-yellow-500 text-center mt-3'>Create a New Account</Link>
       <p className='font-light text-slate-500 text-xs mt-3'>By signing in, you agree to IMDbs Conditions of Use and Privacy Policy.</p>
     </div>
     <div className='flex flex-col mt-8 mb-8'>
